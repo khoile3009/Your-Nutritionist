e7998eebcd30f97b26150b66a8fe1c11e59d9f3e
Khoi Le <khoibaka@gmail.com>
2020-04-28 12:38:06 -0400

    Add Step, Ingredient amount,

--- your_nutritionist/cookbook/models.py
+++ your_nutritionist/cookbook/models.py
@@ -39,3 +39,44 @@ class IngredientsNutrition(models.Model):
     def __str__(self):
         return f'{self.ingredient_id}'
 
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    cook_time = models.IntegerField()
+    # creator_id = models.ForeignKey('User')  #Authentication later
+    number_person = models.IntegerField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class IngredientAmount(models.Model):
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    MEASUREMENT = (
+        (0, 'weight'),
+        (1, 'volume')
+    )
+    measurement = models.BinaryField(choices=MEASUREMENT)
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['amount']
+
+    def __str__(self):
+        return f'{self.recipe_id} {self.ingredient_id}'
+
+
+class Step(models.Model):
+    order = models.IntegerField()
+    time = models.IntegerField()  # in minute
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f'{self.recipe_id} {self.order}'
