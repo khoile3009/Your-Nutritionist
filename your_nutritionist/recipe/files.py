6ba235f0ba5b431c72df2f4bc88f3c605a7080e8
Khoi Le <khoibaka@gmail.com>
2020-06-25 15:35:32 -0400

    add viable_file_name method to GCLOUD

--- your_nutritionist/recipe/files.py
+++ your_nutritionist/recipe/files.py
@@ -1,31 +1,52 @@
 from google.cloud import storage
 import os
 
+
+
 class GCLOUD:
+
 
     @staticmethod
     def upload_and_return_url(user_id,files):
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\khoib\Projects\cookery-281115-d71bfc9ab974.json'
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\khoib\Projects\gcloud_privatekey.json'
         client = storage.Client()
         bucket = client.get_bucket('mediastorage-cookery')
         urls = []
         for name,file in files.items():
             print(file.content_type)
-            blob = bucket.blob(str(user_id) + '/' + file.name)
+            file_name = GCLOUD.__viable_file_name(bucket, str(user_id) + '/' + file.name)
+            print(file_name)
+            blob = bucket.blob(file_name)
             blob.upload_from_file(file, content_type=file.content_type)
-            urls.append('https://media.w3.org/2010/05/sintel/trailer_hd.mp4')
+            urls.append(blob.public_url)
         return urls
 
-
+    @staticmethod
+    def __viable_file_name(bucket, file_name):
+        splitted_file_name = os.path.splitext(file_name)
+        if(bucket.get_blob(file_name) == None):
+            return file_name
+        else:
+            i = 1
+            while(bucket.get_blob(splitted_file_name[0] + '(' + str(i) + ')' + splitted_file_name[1]) != None):
+                i+=1
+        return splitted_file_name[0] + '(' + str(i) + ')' + splitted_file_name[1]
 
     @staticmethod
-    def get_signed_url():
-        pass
+    def get_signed_url(public_path):
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'gcloud_privatekey.json'
+        client = storage.Client()
+        bucket = client.get_bucket('mediastorage-cookery')
+        # blob.get
 
-# def __viable_file_name(bucket, file_name):
-#     if(bucket.get_blob(file_name) == None):
-#         return file_name
-#     else:
-#         i = 1
-#         while()
+def __viable_file_name(bucket, file_name):
+    splitted_file_name = os.path.splittext(file_name)
+    if(bucket.get_blob(file_name) == None):
+        return file_name
+    else:
+        i = 1
+        while(bucket.get_blob(splitted_file_name[0] + '(' + str(i) + ')' + splitted_file_name[1]) != None):
+            i+=1
+    return splitted_file_name[0] + '(' + str(i) + ')' + splitted_file_name[1]
+
     
