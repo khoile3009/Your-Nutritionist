4d87045f8593563c01928c395e5656f02027e420
Khoi Le <khoibaka@gmail.com>
2020-06-26 17:30:42 -0400

    Remove excess media code in branch ui

--- your_nutritionist/recipe/files.py
+++ your_nutritionist/recipe/files.py
@@ -5,14 +5,14 @@ class GCLOUD:
 
     @staticmethod
     def upload_and_return_url(user_id,files):
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\khoib\Projects\cookery-281115-d71bfc9ab974.json'
-        client = storage.Client()
-        bucket = client.get_bucket('mediastorage-cookery')
+        # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\khoib\Projects\cookery-281115-d71bfc9ab974.json'
+        # client = storage.Client()
+        # bucket = client.get_bucket('mediastorage-cookery')
         urls = []
         for name,file in files.items():
-            print(file.content_type)
-            blob = bucket.blob(str(user_id) + '/' + file.name)
-            blob.upload_from_file(file, content_type=file.content_type)
+            # print(file.content_type)
+            # blob = bucket.blob(str(user_id) + '/' + file.name)
+            # blob.upload_from_file(file, content_type=file.content_type)
             urls.append('https://media.w3.org/2010/05/sintel/trailer_hd.mp4')
         return urls
 
