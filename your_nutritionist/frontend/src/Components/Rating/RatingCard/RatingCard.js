5f5dafee142c0fbe3a8e0ad585ab60a1a968393a
HA <hoanganh.theodore@icloud.com>
2020-06-26 01:35:59 -0400

    implement userId linking for username in comment section

--- your_nutritionist/frontend/src/Components/Rating/RatingCard/RatingCard.js
+++ your_nutritionist/frontend/src/Components/Rating/RatingCard/RatingCard.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './RatingCard.css'
+import { Link } from 'react-router-dom'
+
 const RatingCard = (props) =>{
     return <div className='card rating-card'>
         <p><span id="bold">Rating:</span> {props.rating.rating}</p>
-        <p><span id="bold">User:</span> {props.rating.name}</p>
+        {console.log(props.rating)}
+        <p><span id="bold">User:</span>
+            <Link to={'/user/'+props.rating.user_id}>
+                {props.rating.name}
+            </Link>
+        </p>
         <p><span id="bold">Comment:</span> {props.rating.comment}</p>
     </div>
 }
