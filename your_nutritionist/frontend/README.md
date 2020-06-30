19910b177066366ee6310647560afd4d11e98260
HA <hoanganh.theodore@icloud.com>
2020-06-30 12:29:52 -0400

    UI polish noon push
    Cumulative changes for UI elements, edit frontend README to store style theme variables. added palette.css for styling convenience

--- your_nutritionist/frontend/README.md
+++ your_nutritionist/frontend/README.md
@@ -1,68 +1,30 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# App specs and theme
 
-## Available Scripts
+## 1. Color palette:
 
-In the project directory, you can run:
+- Dark Primary: #0097A7
+- Light Primary: #B2EBF2
+- Primary: #00BCD4
+- Text/Icons: #FFFFFF
+- Accent: #FF5252
+- Text Primary: #212121
+- Text Secondary: #757575
+- Divider: #BDBDBD
 
-### `npm start`
+[CSS master file](src/assets/palette.css)
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## 2. Fonts:
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+- Header: Lobster
+- Header/Navigation: Catamaran
+- Header/Dropdown: Catamaran
+- Title: Noto Sans (probably gonna change to Catamaran or Sarabun)
+- Subtitle: Sarabun
 
-### `npm test`
+```css
+@import url("https://fonts.googleapis.com/css?family=Lobster");
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+@import url("https://fonts.googleapis.com/css2?family=Catamaran:wght@600&display=swap");
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Sarabun:wght@100;200;300;400;700&display=swap");
+```
--- your_nutritionist/frontend/src/Components/NewRecipe/NewRecipe.js
+++ your_nutritionist/frontend/src/Components/NewRecipe/NewRecipe.js
@@ -1,132 +1,160 @@
-import React from 'react';
-import { Container, Row, Col, Form, FormGroup, Button, FormFile } from 'react-bootstrap';
-import './NewRecipe.css';
-import { IngredientFormSection, StepFormSection, ImageForm } from './FormSections/FormSections';
+import React from "react";
+import { Container, Row, Col, Form, FormGroup, Button, FormFile } from "react-bootstrap";
+import "./NewRecipe.css";
+import { IngredientFormSection, StepFormSection, ImageForm } from "./FormSections/FormSections";
 
 const NewRecipe = (props) => {
-    console.log(props.medias)
-    return <>
-        <Row>
-            <Col>
-                <p className='create-recipe-title text-center' > Create New Recipe </p>
-            </Col>
-        </Row>
-        <Form>
-            <Form.Group >
-                <Form.Label><p className='bold'>Recipe Name</p></Form.Label>
-                <Form.Control type="text" placeholder="Recipe Name" name="name" onChange={props.handleChangeSameName} value={props.name} />
-            </Form.Group>
-            <Row>
-                <Col>
-                    <Form.Group >
-                        <Form.Label><p className='bold'>Number of serving</p></Form.Label>
-                        <Form.Control type="number" placeholder="E.g. 1 person" name="number_person" onChange={props.handleChangeSameName} value={props.number_person} />
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group >
-                        <Form.Label><p className='bold'>Prep Time (mins)</p></Form.Label>
-                        <Form.Control type="number" placeholder="E.g. 1" name="prep_time" onChange={props.handleChangeSameName} value={props.prep_time} />
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group>
-                        <Form.Label><p className='bold'>Cook Time (mins)</p></Form.Label>
-                        <Form.Control type="number" placeholder="E.g. 1" name="cook_time" onChange={props.handleChangeSameName} value={props.cook_time} />
-                    </Form.Group>
-                </Col>
-            </Row>
-            <Form.Group>
-                <Form.Label><p className='bold'>Description</p></Form.Label>
-                <Form.Control as="textarea" placeholder="Your description goes here" name="description" value={props.description} onChange={props.handleChangeSameName} rows="3" />
-            </Form.Group>
+	console.log(props.medias);
+	return (
+		<>
+			<Row>
+				<Col>
+					<p className="create-recipe-title text-center"> Create New Recipe </p>
+				</Col>
+			</Row>
+			<Form>
+				<Form.Group>
+					<Form.Label>
+						<p className="bold">Recipe Name</p>
+					</Form.Label>
+					<Form.Control type="text" placeholder="Recipe Name" name="name" onChange={props.handleChangeSameName} value={props.name} />
+				</Form.Group>
+				<Row>
+					<Col>
+						<Form.Group>
+							<Form.Label>
+								<p className="bold">Number of serving</p>
+							</Form.Label>
+							<Form.Control type="number" placeholder="E.g. 1 person" name="number_person" onChange={props.handleChangeSameName} value={props.number_person} />
+						</Form.Group>
+					</Col>
+					<Col>
+						<Form.Group>
+							<Form.Label>
+								<p className="bold">Prep Time (mins)</p>
+							</Form.Label>
+							<Form.Control type="number" placeholder="E.g. 1" name="prep_time" onChange={props.handleChangeSameName} value={props.prep_time} />
+						</Form.Group>
+					</Col>
+					<Col>
+						<Form.Group>
+							<Form.Label>
+								<p className="bold">Cook Time (mins)</p>
+							</Form.Label>
+							<Form.Control type="number" placeholder="E.g. 1" name="cook_time" onChange={props.handleChangeSameName} value={props.cook_time} />
+						</Form.Group>
+					</Col>
+				</Row>
+				<Form.Group>
+					<Form.Label>
+						<p className="bold">Description</p>
+					</Form.Label>
+					<Form.Control as="textarea" placeholder="Your description goes here" name="description" value={props.description} onChange={props.handleChangeSameName} rows="3" />
+				</Form.Group>
 
-            <hr></hr>
+				<hr></hr>
 
-            <Form.Group>
-                <Form.Label><p className='bold'>Images and Videos</p></Form.Label>
-                {props.medias.map((media, index) => {
-                    return <ImageForm
-                        onChangeImage={(event) => { props.onChangeImage(event, index) }}
-                        deleteMedia={() => { props.deleteMedia(index) }}
-                        handleChangeMediaType={(event)=>{props.handleChangeMediaType(event,index)}}
-                        handleChangeMediaUrl={(event)=>{props.handleChangeMediaUrl(event, index)}}
-                        handleChangeMediaName={(event)=>{props.handleChangeMediaName(event, index)}}
-                        handleChangeMediaFile={(event)=>{props.handleChangeMediaFile(event, index)}}
-                        media={media}
-                    ></ImageForm>;
-                })}
-            </Form.Group>
-            <Button variant="outline-dark" className="circle-button center" onClick={props.addMedia}> + </Button>
+				<Form.Group>
+					<Form.Label>
+						<p className="bold">Images and Videos</p>
+					</Form.Label>
+					{props.medias.map((media, index) => {
+						return (
+							<ImageForm
+								onChangeImage={(event) => {
+									props.onChangeImage(event, index);
+								}}
+								deleteMedia={() => {
+									props.deleteMedia(index);
+								}}
+								handleChangeMediaType={(event) => {
+									props.handleChangeMediaType(event, index);
+								}}
+								handleChangeMediaUrl={(event) => {
+									props.handleChangeMediaUrl(event, index);
+								}}
+								handleChangeMediaName={(event) => {
+									props.handleChangeMediaName(event, index);
+								}}
+								handleChangeMediaFile={(event) => {
+									props.handleChangeMediaFile(event, index);
+								}}
+								media={media}
+							></ImageForm>
+						);
+					})}
+				</Form.Group>
+				<Button variant="outline-dark" className="circle-button center" onClick={props.addMedia}>
+					{" "}
+					+{" "}
+				</Button>
 
+				<hr></hr>
 
-            <hr></hr>
+				<Form.Label>
+					<p className="bold">Ingredients</p>
+				</Form.Label>
+				{props.ingredient_sections.map((ingredient_section, index) => {
+					return <IngredientFormSection units={props.units} addIngredient={props.addIngredient} section_index={index} ingredient_section={ingredient_section} handleChangeIngredient={props.handleChangeIngredient} deleteIngredient={props.deleteIngredient} deleteIngredientSection={props.deleteIngredientSection} handleChangeIngredientSectionName={props.handleChangeIngredientSectionName}></IngredientFormSection>;
+				})}
+				<Button variant="light" className=" center" onClick={props.addIngredientSection}>
+					{" "}
+					Add section{" "}
+				</Button>
 
-            <Form.Label><p className='bold'>Ingredients</p></Form.Label>
-            {props.ingredient_sections.map((ingredient_section, index) => {
-                return <IngredientFormSection
-                    units={props.units}
-                    addIngredient={props.addIngredient}
-                    section_index={index}
-                    ingredient_section={ingredient_section}
-                    handleChangeIngredient={props.handleChangeIngredient}
-                    deleteIngredient={props.deleteIngredient}
-                    deleteIngredientSection={props.deleteIngredientSection}
-                    handleChangeIngredientSectionName={props.handleChangeIngredientSectionName}
-                ></IngredientFormSection>
-            })}
-            <Button variant="light" className=" center" onClick={props.addIngredientSection}> Add section </Button>
+				<hr></hr>
 
-            <hr></hr>
+				<Form.Label>
+					<p className="bold">Steps</p>
+				</Form.Label>
+				{props.step_sections.map((step_section, index) => {
+					return (
+						<StepFormSection
+							addStep={props.addStep}
+							section_index={index}
+							step_section={step_section}
+							deleteStep={props.deleteStep}
+							deleteStepSection={props.deleteStepSection}
+							handleChangeStepSectionName={props.handleChangeStepSectionName}
+							handleChangeStepMediaId={props.handleChangeStepMediaId}
+							handleChangeStepTimestamp={props.handleChangeStepTimestamp}
+							handleChangeStepDirection={props.handleChangeStepDirection}
+							imageNames={props.imageNames}
+							videoOptions={props.medias
+								.map((media, index) => {
+									console.log(media);
+									if (media.type == 2 || media.type == 3) {
+										return {
+											index: index,
+											name: media.name,
+										};
+									}
+								})
+								.filter((media) => media)}
+						></StepFormSection>
+					);
+				})}
+				<Button variant="light" className=" center" onClick={props.addStepSection}>
+					{" "}
+					Add section{" "}
+				</Button>
 
-            <Form.Label><p className='bold'>Steps</p></Form.Label>
-            {props.step_sections.map((step_section, index) => {
-                return <StepFormSection
-                    addStep={props.addStep}
-                    section_index={index}
-                    step_section={step_section}
-                    deleteStep={props.deleteStep}
-                    deleteStepSection={props.deleteStepSection}
-                    handleChangeStepSectionName={props.handleChangeStepSectionName}
-                    handleChangeStepMediaId={props.handleChangeStepMediaId}
-                    handleChangeStepTimestamp={props.handleChangeStepTimestamp}
-                    handleChangeStepDirection={props.handleChangeStepDirection}
-                    imageNames={props.imageNames}
-                    videoOptions={
-                        props.medias.map(
-                            (media, index)=>{
-                                console.log(media)
-                                if(media.type == 2 || media.type == 3){
-                                    return {
-                                        index: index,
-                                        name: media.name
-                                    }
-                                } 
-                            }
-                        ).filter(
-                            (media) => (media)
-                        )
-                    }
-                ></StepFormSection>
-            })}
-            <Button variant="light" className=" center" onClick={props.addStepSection}> Add section </Button>
-
-            <hr></hr>
-
-
-            
-            <Button className="createRecipeBtn" onClick={props.submitForm}>Create</Button>
-            <Button className="cancelRecipeBtn" variant='light' >Cancel</Button>
-
-            
-
-        </Form>
-
-
-
-
-
-    </>
-}
+				<hr></hr>
+				<Row className="submitCancelBtn">
+					<Col></Col>
+					<Col className="target">
+						<Button className="createRecipeBtn" onClick={props.submitForm}>
+							Create
+						</Button>
+						<Button className="cancelRecipeBtn" href="/home">
+							Cancel
+						</Button>
+					</Col>
+					<Col></Col>
+				</Row>
+			</Form>
+		</>
+	);
+};
 
 export default NewRecipe;
