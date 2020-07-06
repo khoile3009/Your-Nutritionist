2f636f70f23319133c08f72716d134063dab93ad
Khoi Le <khoibaka@gmail.com>
2020-07-06 15:31:32 -0400

    Finish api for profile picture

--- your_nutritionist/accounts/admin.py
+++ your_nutritionist/accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import UserHeadline, UserIntroduction
+from .models import UserHeadline, UserIntroduction, UserProfilePic
 # Register your models here.
 
 
 admin.site.register(UserHeadline)
 admin.site.register(UserIntroduction)
+admin.site.register(UserProfilePic)
--- your_nutritionist/accounts/models.py
+++ your_nutritionist/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete
+from recipe.files import GCLOUD
 # Create your models here.
 
 class UserHeadline(models.Model):
@@ -13,5 +15,14 @@ class UserIntroduction(models.Model):
 
 class UserProfilePic(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    profile_pic = models.CharField(max_length=100)
+    url = models.CharField(max_length=1000)
+    gcloud_bucket_url = models.CharField(max_length=500)
+    
 
+def on_delete(sender, instance, **kwargs):
+    if(instance.gcloud_bucket_url != ''):
+        print('on delete ' + instance.gcloud_bucket_url)
+        GCLOUD.delete_bucket_url(bucket_url=instance.gcloud_bucket_url)
+
+
+pre_delete.connect(on_delete, sender=UserProfilePic)
