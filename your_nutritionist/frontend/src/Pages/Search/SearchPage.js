8de93f8932e9781d27a9b21f279f0395c7f22de1
HA <hoanganh.theodore@icloud.com>
2020-07-14 14:41:30 -0400

    fixed styling for search page completed

--- your_nutritionist/frontend/src/Pages/Search/SearchPage.js
+++ your_nutritionist/frontend/src/Pages/Search/SearchPage.js
@@ -1,125 +1,106 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import SigninRequired from '../../Containers/SigninRequired/SigninRequired'
-import NewFeedContainer from '../../Containers/NewFeed/NewFeedContainer'
-import RecipeSearchbox from '../../Containers/RecipeSearchbox/RecipeSearchbox'
-import RecipeList from '../../Containers/RecipeList/RecipeList'
+import SigninRequired from '../../Containers/SigninRequired/SigninRequired';
+import NewFeedContainer from '../../Containers/NewFeed/NewFeedContainer';
+import RecipeSearchbox from '../../Containers/RecipeSearchbox/RecipeSearchbox';
+import RecipeList from '../../Containers/RecipeList/RecipeList';
 import queryString from 'query-string';
 import axios from '../../axios-orders';
-import { withRouter, useLocation } from 'react-router-dom'
+import { withRouter, useLocation } from 'react-router-dom';
 import { compose } from 'redux';
-import "./SearchPage.css";
-
+import './SearchPage.css';
 
 class SearchPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			recipes: null,
+			search_query: '',
+			page: 1
+		};
+		this.setRecipes = this.setRecipes.bind(this);
+		this.setSearchQuery = this.setSearchQuery.bind(this);
+		this.search = this.search.bind(this);
+		this.toPage = this.toPage.bind(this);
+	}
 
-    constructor(props){
-        super(props)
-        this.state = {
-            recipes : null,
-            search_query: '',
-            page: 1,
-        }
-        this.setRecipes = this.setRecipes.bind(this)
-        this.setSearchQuery = this.setSearchQuery.bind(this)
-        this.search = this.search.bind(this)
-        this.toPage = this.toPage.bind(this)
-    }
+	componentDidMount = () => {
+		console.log(this.state.page);
+		if (this.props.location.search) {
+			let params = queryString.parse(this.props.location.search);
+			let query = params.query;
+			axios
+				.get('api/recipe', {
+					params: {
+						query: query,
+						block: Math.floor(parseInt(params.page - 1) / 5)
+					}
+				})
+				.then((response) => {
+					this.setState({
+						recipes: response.data.recipes,
+						query: query,
+						page: parseInt(params.page)
+					});
+				});
+			this.setState({});
+		}
+		this.num_preload = 5;
+	};
 
-    componentDidMount = () => {
-        console.log(this.state.page)
-        if(this.props.location.search){
-            let params = queryString.parse(this.props.location.search)
-            let query = params.query
-            axios.get('api/recipe', {
-                params: {
-                    query: query,
-                    block: Math.floor(parseInt(params.page - 1) / 5)
-                }
-            })
-            .then(
-                (response) => {
-                    this.setState({
-                        recipes: response.data.recipes,
-                        query: query,
-                        page: parseInt(params.page)
-                    })
-                }
-            )
-            this.setState({})
-        }
-        this.num_preload = 5
-    }
+	componentWillReceiveProps(props) {
+		let next_params = queryString.parse(props.location.search);
+		let next_page = parseInt(next_params.page);
+		if (Math.floor((next_page - 1) / this.num_preload) !== Math.floor((this.state.page - 1) / this.num_preload)) {
+			axios
+				.get('api/recipe', {
+					params: {
+						query: next_params.query,
+						block: Math.floor((next_page - 1) / this.num_preload)
+					}
+				})
+				.then((response) => {
+					this.setState({
+						recipes: response.data.recipes,
+						query: next_params.query,
+						page: next_page
+					});
+				});
+		} else {
+			this.setState({
+				page: next_page
+			});
+		}
+	}
 
-    componentWillReceiveProps(props){   
-        let next_params = queryString.parse(props.location.search)
-        let next_page = parseInt(next_params.page);
-        if(Math.floor((next_page - 1)/ this.num_preload) !== Math.floor((this.state.page - 1)/ this.num_preload)){
-            axios.get('api/recipe', {
-                params: {
-                    query: next_params.query,
-                    block: Math.floor((next_page - 1)/ this.num_preload)
-                }
-            })
-            .then(
-                (response) => {
-                    this.setState({
-                        recipes: response.data.recipes,
-                        query: next_params.query,
-                        page: next_page
-                    })
-                }
-            )
-        }
-        else{
-            this.setState({
-                page: next_page
-            })
-        }
-        
-    }
+	setSearchQuery = (event) => {
+		this.setState({ query: event.target.value });
+	};
 
-    setSearchQuery = (event) => {
-        this.setState({query: event.target.value})
-    }
+	search = () => {
+		this.props.history.push({ pathname: '/search', search: queryString.stringify({ query: this.state.query, page: 1 }) });
+	};
 
-    search = () => {
-        this.props.history.push({pathname: '/search', search: queryString.stringify({query: this.state.query, page: 1})})
-    }
+	setRecipes = (recipes) => {
+		this.setState({ recipes: recipes });
+	};
 
-    setRecipes = (recipes) => {
-        this.setState({recipes : recipes})
-    }
+	toPage = (page) => {
+		console.log(page);
+		this.props.history.push({ pathname: '/search', search: queryString.stringify({ query: queryString.parse(this.props.location.search).query, page: page }) });
+	};
 
-    toPage = (page) => {
-        console.log(page)
-        this.props.history.push({pathname: '/search', search: queryString.stringify({query: queryString.parse(this.props.location.search).query, page: page})})
-    }
-
-    render() {
-        return <>
-            <RecipeSearchbox
-                setRecipes = {this.setRecipes}
-                setSearchQuery ={this.setSearchQuery}
-                query={this.state.query}
-                search={this.search}
-                
-            ></RecipeSearchbox>
-            <Container className="search-result-container">
-                <h2>Search results</h2>
-                <br></br>
-                {this.state.recipes
-                ? <RecipeList
-                    recipes={this.state.recipes}
-                    page={this.state.page}
-                    toPage={this.toPage}
-                ></RecipeList>
-                :null
-            }
-            </Container>
-
-
-        </>
-    }
+	render() {
+		return (
+			<div className="search-page-wrapper">
+				<RecipeSearchbox setRecipes={this.setRecipes} setSearchQuery={this.setSearchQuery} query={this.state.query} search={this.search} />
+				<div className="search-result-container">
+					<h2>Search results</h2>
+					<br />
+					{this.state.recipes ? <RecipeList recipes={this.state.recipes} page={this.state.page} toPage={this.toPage} /> : null}
+				</div>
+			</div>
+		);
+	}
 }
 export default withRouter(SearchPage);
