f2f0305519196c92605a51b2bdd9948d986c1f84
HA <hoanganh.theodore@icloud.com>
2020-07-16 13:58:43 -0400

    complete restyle of rating form

--- your_nutritionist/frontend/src/Components/Rating/RatingFormCard/RatingFormCard.js
+++ your_nutritionist/frontend/src/Components/Rating/RatingFormCard/RatingFormCard.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
-import './RatingFormCard.css'
+import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import "./RatingFormCard.css";
 const RatingFormCard = (props) => {
-    console.log(props)
-    return <Form className='card' onSubmit={props.submitRating}>
+    console.log(props);
+    return (
+        <Form className="card" onSubmit={props.submitRating}>
             <Form.Group controlId="formBasicRating">
-                <Form.Label>Rating</Form.Label>
-                <Form.Control type="number" placeholder="Enter rating" value={props.your_rating.rating} onChange={props.handleRatingChange}/>
-                <Form.Label>Comment</Form.Label>
-                <Form.Control as='textarea' rows='3' placeholder="Comment" value={props.your_rating.comment} onChange={props.handleCommentChange} />
+                <Form.Label className="rating-lbl">Rating</Form.Label>
+                <Form.Control
+                    className="rating-score input-field"
+                    type="number"
+                    placeholder="Enter rating"
+                    value={props.your_rating.rating}
+                    onChange={props.handleRatingChange}
+                />
+                <Form.Label className="rating-lbl">Comment</Form.Label>
+                <Form.Control
+                    className="rating-content input-field"
+                    as="textarea"
+                    rows="3"
+                    placeholder="Comment"
+                    value={props.your_rating.comment}
+                    onChange={props.handleCommentChange}
+                />
             </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Submit
-        </Button>
-    </Form>
-}
-export default RatingFormCard 
+            <Row>
+                <Col></Col>
+                <Col md="auto" xs="auto">
+                    <Button className="rating-submit-btn" type="submit">
+                        Submit
+                    </Button>
+                </Col>
+                <Col></Col>
+            </Row>
+        </Form>
+    );
+};
+export default RatingFormCard;
