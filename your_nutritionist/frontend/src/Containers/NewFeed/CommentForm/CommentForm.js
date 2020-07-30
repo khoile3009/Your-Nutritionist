4bfc697ef3b4e3e139a66a342afdb5c89dbbc7a7
Khoi Le <khoibaka@gmail.com>
2020-07-30 12:04:56 -0400

    Comment functional

---
+++ your_nutritionist/frontend/src/Containers/NewFeed/CommentForm/CommentForm.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import {Form, InputGroup, Button, FormControl} from 'react-bootstrap'
+
+class CommentForm extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            comment: ''
+        }
+        this.onChangeHandler = this.onChangeHandler.bind(this)
+    }
+
+    onChangeHandler = (event) => {
+        this.setState({comment: event.target.value})
+    }
+
+    render(){
+        return <div className='comment-form'>
+            <Form onSubmit={(event)=>{this.props.submitComment(event,this.state.comment)}}>
+                <InputGroup>          
+                    <FormControl as='textarea' rows='2' name='comment' value={this.state.comment} onChange={this.onChangeHandler}/>
+                    <InputGroup.Append>
+                        <Button type='submit' variant='primary'>Submit</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </Form>
+        </div>;
+    }
+}
+
+export default CommentForm;
--- your_nutritionist/frontend/src/Containers/NewFeed/PostCard/PostCardContainer.js
+++ your_nutritionist/frontend/src/Containers/NewFeed/PostCard/PostCardContainer.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import { PostContent, PostInteraction, PostMedia, PostProfilePic, PostUsername } from '../../../Components/NewFeed/PostCard/index';
 import { withRouter } from 'react-router-dom';
 import axios from '../../../axios-orders';
+import CommentSection from '../CommentSection/CommentSection'
 class PostCardContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
             currentMedia: 0,
-            comments: []
+            comments: [],
+            showComments: false,
+            last_id: -1,
+            post: this.props.post 
         }
         this.toCreator = this.toCreator.bind(this)
         this.loadComment = this.loadComment.bind(this)
+        this.toggleCommentSection = this.toggleCommentSection.bind(this)
+        this.toggleLike = this.toggleLike.bind(this)
     }
 
     toCreator = (event) => {
         event.preventDefault();
         event.stopPropagation();
-        this.props.history.push('/user/' + this.props.post.user_id)
+        this.props.history.push('/user/' + this.state.post.user_id)
     }
 
     loadComment = () => {
-        axios.get('api/post/' + this.props.post.post_id + '/comment', {params:{before_id: -1, }}).then(
+        axios.get('api/post/' + this.state.post.post_id + '/comment', {params:{before_id: this.state.last_id, }}).then(
           (response) => {
               let comments = this.state.comments
               console.log(response)
@@ -34,31 +40,67 @@ class PostCardContainer extends Component {
         )
     }
 
+    toggleLike = () => {
+        this.setState({post: {
+            ...this.state.post,
+            num_like: this.state.post.num_like + this.state.post.liked ? -1 : 1,
+            liked: !this.state.post.liked
+        }})
+    }
+
+    toggleCommentSection = () => {
+        if(this.state.showComments){
+            this.setState({comments: [], showComments: false})
+        }
+        else{
+            axios.get('api/post/' + this.state.post.post_id + '/comment', {params:{before_id: -1, }}).then(
+                (response) => {
+                    let comments = this.state.comments
+                    console.log(response)
+                    comments.push.apply(comments, response.data.comments)
+                  this.setState({comments: comments, showComments: true})
+                }
+              ).catch(
+                  (error) => {
+                      console.log(error)
+                  }
+            )
+        }
+        
+    }
+
+
     render() {
-        console.log(this.state.comments)
         return <div className="card newfeedcard-wrapper container">
             <PostProfilePic
-                profilepic={this.props.post.profilepic}
+                profilepic={this.state.post.profilepic}
                 toCreator={this.toCreator}
             />
             <PostUsername
-                username={this.props.post.username}
+                username={this.state.post.username}
                 toCreator={this.toCreator}
             />
             <PostContent 
-            content={this.props.post.content}
+            content={this.state.post.content}
             />
-            {this.props.post.medias && this.props.post.medias.length != 0
+            {this.state.post.medias && this.state.post.medias.length != 0
                 ? <PostMedia/>
                 : null
             }
             
             <PostInteraction 
-                num_like = {this.props.post.num_like}
-                num_comment = {this.props.post.num_comment}
+                liked = {this.state.post.liked}
+                num_like = {this.state.post.num_like}
+                num_comment = {this.state.post.num_comment}
                 loadComment = {this.loadComment}
+                toggleLike = {this.toggleLike}
+                toggleCommentSection = {this.toggleCommentSection}
             />
-
+            {
+                this.state.showComments
+                ? <CommentSection comments={this.state.comments} post_id={this.state.post.post_id}/>
+                :null
+            }
         </div>
     }
 }
