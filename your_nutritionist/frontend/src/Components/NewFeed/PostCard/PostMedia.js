466144ef632859b9c322500f4377d3c19d22540a
Khoi Le <khoibaka@gmail.com>
2020-07-30 16:02:37 -0400

    Finish media section of post need styling

--- your_nutritionist/frontend/src/Components/NewFeed/PostCard/PostMedia.js
+++ your_nutritionist/frontend/src/Components/NewFeed/PostCard/PostMedia.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './NewFeedCard.scss';
-
-const PostMedia = (props) => {
-	return (
-		<div className="newfeed-media-wrapper">
-				<div className="newfeed-media" />
-		</div>
-	);
+import ReactPlayer from 'react-player';
+const PostImage = (props) => {
+	return <div className='post-media-content' style={{
+        backgroundImage: "url('" + props.url + "')",
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+    }}/>
 };
 
+const PostVideo = (props) => {
+	return <div className='post-media-content'>
+		<ReactPlayer
+            url={props.url}
+            height='100%'
+            width='100%'
+            controls={true}
+        />
+	</div>
+};
+
+const PostMedia = (props) => {
+	return props.media.type == 2 || props.media.type == 3
+		? <PostVideo url={props.media.url}/>
+		: <PostImage url={props.media.url}/>
+	
+}
 export default PostMedia;
