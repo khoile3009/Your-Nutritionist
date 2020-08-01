478c3cae6bcc1a7642c5af53578b5a199bc16193
HA <hoanganh.theodore@icloud.com>
2020-08-01 19:57:15 -0400

    new logo, removed brand name link on header

--- your_nutritionist/frontend/src/Components/Util/Header/Header.js
+++ your_nutritionist/frontend/src/Components/Util/Header/Header.js
@@ -1,30 +1,26 @@
-import { Navbar, Nav } from 'react-bootstrap';
-import React from 'react';
-import { Link, NavLink, Route } from 'react-router-bootstrap';
-import './Header.scss';
+import { Navbar, Nav } from "react-bootstrap";
+import React from "react";
+import { Link, NavLink, Route } from "react-router-bootstrap";
+import "./Header.scss";
 
 const Header = (props) => (
 	<div className="nav-wrapper">
 		<div className="header-background" />
 		<Navbar className="color-nav" collapseOnSelect expand="lg">
 			{/* "Link" in brand component since just redirect is needed */}
-			<Navbar.Brand as={Link} to="/" id="brand-color">
+			<Navbar.Brand className="logo" onClick={props.toHomePage} as={Link} to="/homepage" id="brand-color">
 				<img
-					src={'https://storage.googleapis.com/your-nutritionist-cdn/Cookery.png'}
+					src={"https://storage.googleapis.com/your-nutritionist-cdn/recipe-repo.png"}
 					style={{
-						width: 50,
-						margin: 5
+						width: 200,
 					}}
 				/>
-				<span id="brand-title">
-					<a href="/homepage">Cookery</a>
-				</span>
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 			<Navbar.Collapse id="responsive-navbar-nav">
 				<Nav className="mr-auto">
 					{/* "NavLink" here since "active" class styling is needed */}
-					<Nav.Link style={{ display: 'none' }} onClick={props.toCreateRecipe} href="/recipe/create">
+					<Nav.Link style={{ display: "none" }} onClick={props.toCreateRecipe} href="/recipe/create">
 						Create Recipe
 					</Nav.Link>
 					<form onSubmit={props.searchSubmit}>
