ba3b62a81e86b4b401bbd850dffbb44c1a834ec5
HA <hoanganh.theodore@icloud.com>
2020-08-05 21:41:28 -0400

    Auto stash before revert of "Merge remote-tracking branch 'origin/ui' into ui"

--- your_nutritionist/frontend/src/Containers/Util/MediaUpload/MediaInput/MediaInput.js
+++ your_nutritionist/frontend/src/Containers/Util/MediaUpload/MediaInput/MediaInput.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import './MediaInput.scss';
-import { Button } from 'react-bootstrap'
-import ReactPlayer from 'react-player';
+import React, { Component } from "react";
+import "./MediaInput.scss";
+import { Button } from "react-bootstrap";
+import ReactPlayer from "react-player";
 class MediaInput extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            media: null,
-            file: null,
-            url: '',
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			media: null,
+			file: null,
+			url: "",
+		};
+	}
 
+<<<<<<< Updated upstream
     componentDidMount() {
         console.log(this.props)
         this.setState({
@@ -19,112 +20,150 @@ class MediaInput extends Component {
             url: '',
             file: this.props.file
         })
+=======
+	componentWillReceiveProps(props) {
+		this.setState({
+			media: props.media,
+			url: "",
+			file: props.file,
+		});
+	}
+>>>>>>> Stashed changes
 
-    }
+	submitFile = (event, type) => {
+		var getImagePath = URL.createObjectURL(event.target.files[0]);
+		if (event.target.files && event.target.files.length) {
+			this.setState({
+				media: {
+					type: type,
+					url: getImagePath,
+				},
+				file: event.target.files[0],
+				editing: false,
+			});
+		}
+	};
 
-    submitFile = (event, type) => {
-        var getImagePath = URL.createObjectURL(event.target.files[0])
-        if (event.target.files && event.target.files.length) {
-            this.setState({
-                media: {
-                    type: type,
-                    url: getImagePath
-                },
-                file: event.target.files[0],
-                editing: false
-            })
-        }
-    }
+	submitUrl = (type) => {
+		this.setState({
+			media: {
+				type: type,
+				url: this.state.url,
+			},
+			editing: false,
+		});
+	};
+	goBackToChooseFile = () => {
+		this.setState({ media: null, file: null });
+	};
 
-    submitUrl = (type) => {
-        this.setState({
-            media: {
-                type: type,
-                url: this.state.url
-            },
-            editing: false
-        })
-    }
-    goBackToChooseFile = () => {
-        this.setState({ media: null, file: null })
-    }
+	urlChange = (event) => {
+		this.setState({ url: event.target.value });
+	};
 
-    urlChange = (event) => {
-        this.setState({ url: event.target.value })
-    }
+	activeToComponent = (active) => {
+		switch (active) {
+			case 0:
+				return (
+					<div>
+						<input
+							type="text"
+							className="media-input-field"
+							value={this.state.url}
+							onChange={this.urlChange}
+						/>
+						<Button
+							onClick={() => {
+								this.submitUrl(0);
+							}}
+						>
+							Submit
+						</Button>
+					</div>
+				);
 
-    activeToComponent = (active) => {
-        switch (active) {
-            case (0):
-                return <div>
-                    <input
-                        type="text"
-                        className='media-input-field'
-                        value={this.state.url}
-                        onChange={this.urlChange} />
-                    <Button onClick={() => { this.submitUrl(0) }}>Submit</Button>
-                </div>
+			case 1:
+				return (
+					<input
+						type="file"
+						className="media-input-field"
+						onChange={(event) => {
+							this.submitFile(event, 1);
+						}}
+						accept="image/x-png,image/gif,image/jpeg"
+					/>
+				);
 
-            case (1):
-                return <input
-                    type="file"
-                    className='media-input-field'
-                    onChange={(event) => { this.submitFile(event, 1) }}
-                    accept='image/x-png,image/gif,image/jpeg' />
+			case 2:
+				return (
+					<div>
+						<input
+							type="text"
+							className="media-input-field"
+							value={this.state.url}
+							onChange={this.urlChange}
+						/>
+						<Button
+							onClick={() => {
+								this.submitUrl(2);
+							}}
+						>
+							Submit
+						</Button>
+					</div>
+				);
 
-            case (2):
-                return <div>
-                    <input
-                        type="text"
-                        className='media-input-field'
-                        value={this.state.url}
-                        onChange={this.urlChange} />
-                    <Button onClick={() => { this.submitUrl(2) }}>Submit</Button>
-                </div>
+			case 3:
+				return (
+					<input
+						type="file"
+						className="media-input-field"
+						onChange={(event) => {
+							this.submitFile(event, 3);
+						}}
+						accept="video/mp4,video/m4v"
+					/>
+				);
+		}
+	};
 
-            case (3):
-                return <input
-                    type="file"
-                    className='media-input-field'
-                    onChange={(event) => { this.submitFile(event, 3) }}
-                    accept='video/mp4,video/m4v'
-                />
-        }
-    }
-
-    render() {
-        return <div className="media-input-wrapper">
-            {this.state.media
-                ? <MediaPreviewBig
-                    addMedia={() => { this.props.addMedia(this.state.media, this.state.file) }}
-                    media={this.state.media}
-                    goBackToChooseFile={this.goBackToChooseFile} />
-                : this.activeToComponent(this.props.active)}
-
-        </div>
-    }
+	render() {
+		return (
+			<div className="media-input-wrapper">
+				{this.state.media ? (
+					<MediaPreviewBig
+						addMedia={() => {
+							this.props.addMedia(this.state.media, this.state.file);
+						}}
+						media={this.state.media}
+						goBackToChooseFile={this.goBackToChooseFile}
+					/>
+				) : (
+					this.activeToComponent(this.props.active)
+				)}
+			</div>
+		);
+	}
 }
 
 const MediaPreviewBig = (props) => {
-    return <>
-        {props.media.type == 0 || props.media.type == 1
-        ? <div
-        className='image-preview-big'
-        style={{ backgroundImage: "url('" + props.media.url + "')" }}>
-        </div>
-        : <div className='video-preview-big'>
-            <ReactPlayer 
-            url={props.media.url}
-            height='100%'
-            width='100%'
-            controls={true}
-            />
-        </div>
-        }
-        
-        <Button onClick={props.addMedia}>Confirm</Button>
-        <Button onClick={props.goBackToChooseFile}>Cancel</Button>
-    </>
-}
+	return (
+		<>
+			{props.media.type == 0 || props.media.type == 1 ? (
+				<div
+					className="image-preview-big"
+					style={{ backgroundImage: "url('" + props.media.url + "')" }}
+				></div>
+			) : (
+				<div className="video-preview-big">
+					<ReactPlayer url={props.media.url} height="100%" width="100%" controls={true} />
+				</div>
+			)}
 
-export default MediaInput
+			<Button onClick={props.addMedia}>Confirm</Button>
+			<Button onClick={props.goBackToChooseFile}>Cancel</Button>
+		</>
+	);
+};
+
+export default MediaInput;
--- your_nutritionist/frontend/src/Containers/Util/MediaUpload/MediaUploadContainer.js
+++ your_nutritionist/frontend/src/Containers/Util/MediaUpload/MediaUploadContainer.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { Component } from 'react'
 import './MediaUploadContainer.css'
 import $ from 'jquery'
@@ -52,8 +53,51 @@ class MediaUploadContainer extends Component {
         this.props.setMedia(medias, files)
     }
 
+=======
+import React, { Component } from "react";
+import "./MediaUploadContainer.scss";
+import $ from "jquery";
+import TabList from "../../../Components/Util/TabList/TabList";
+import MediaInput from "./MediaInput/MediaInput";
+import MediaPreviewList from "./MediaPreviewList/MediaPreviewList";
+class MediaUploadContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			files: [],
+			medias: [],
+			active: 0,
+			current: 0,
+		};
+		this.tabs = ["Image url", "Image file", "Youtube url", "Video file"];
+		this.switchTab = this.switchTab.bind(this);
+		this.getCurrentFile = this.getCurrentFile.bind(this);
+	}
 
+	switchTab = (index) => {
+		console.log(index);
+		this.setState({ active: index });
+	};
 
+	submitNewImageFile = (event) => {
+		if (event.target.files && event.target.files.length)
+			var getImagePath = URL.createObjectURL(event.target.files[0]);
+		$(".file-preview").css("background-image", "url(" + getImagePath + ")");
+	};
+
+	getCurrentMedia = () => {
+		return this.state.current == this.state.medias.length
+			? null
+			: this.state.medias[this.state.current];
+	};
+>>>>>>> Stashed changes
+
+	changeCurrentMedia = (current) => {
+		this.switchTab(current == this.state.medias.length ? 0 : this.state.medias[current].type);
+		this.setState({ current: current });
+	};
+
+<<<<<<< Updated upstream
     getCurrentMedia = () => {
         return this.state.current == this.state.medias.length
             ? null
@@ -110,6 +154,54 @@ class MediaUploadContainer extends Component {
             <MediaPreviewList medias={this.state.medias} changeCurrentMedia={this.changeCurrentMedia} current={this.state.current} />
         </div>
     }
+=======
+	addMedia = (media, file) => {
+		let medias = this.state.medias;
+		let files = this.state.files;
+		if (this.state.current == this.state.medias.length) {
+			medias.push(media);
+			files.push(file);
+		} else {
+			files[this.state.current] = file;
+			medias[this.state.current] = media;
+		}
+
+		this.setState({ files: files, medias: medias, current: medias.length });
+	};
+
+	getCurrentFile = () => {
+		if (this.state.current == this.state.medias.length) {
+			return null;
+		} else {
+			return this.state.files[this.state.current];
+		}
+	};
+
+	render() {
+		console.log(this.state.files);
+		console.log(this.state.medias);
+		return (
+			<div className="media-preview-wrapper">
+				<TabList active={this.state.active} tabs={this.tabs} switchTab={this.switchTab} />
+				<MediaInput
+					current={this.state.current}
+					addMedia={this.addMedia}
+					media={this.getCurrentMedia()}
+					file={this.getCurrentFile()}
+					active={this.state.active}
+					currentFileId={this.state.files.length}
+				/>
+				<div className="media-preview-list-wrapper">
+					<MediaPreviewList
+						medias={this.state.medias}
+						changeCurrentMedia={this.changeCurrentMedia}
+						current={this.state.current}
+					/>
+				</div>
+			</div>
+		);
+	}
+>>>>>>> Stashed changes
 }
 
 export default MediaUploadContainer;
