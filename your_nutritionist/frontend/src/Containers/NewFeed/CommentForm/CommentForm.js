0971ec0be2fbe814d9a1cd65b2463906614ca530
HA <hoanganh.theodore@icloud.com>
2020-08-10 18:29:19 -0400

    implement error check for comment section

--- your_nutritionist/frontend/src/Containers/NewFeed/CommentForm/CommentForm.js
+++ your_nutritionist/frontend/src/Containers/NewFeed/CommentForm/CommentForm.js
@@ -3,52 +3,82 @@ import { Form, InputGroup, Button, FormControl } from "react-bootstrap";
 import "./CommentForm.scss";
 
 class CommentForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			comment: "",
-		};
-		this.onChangeHandler = this.onChangeHandler.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            comment: "",
+            chk: {
+                commentContentChk: null,
+            },
+        };
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
 
-	onChangeHandler = (event) => {
-		this.setState({ comment: event.target.value });
-	};
+    onChangeHandler = (event) => {
+        this.setState({ comment: event.target.value });
+    };
 
-	render() {
-		return (
-			<div className="comment-form">
-				<hr></hr>
-				<Form
-					onSubmit={(event) => {
-						this.props.submitComment(event, this.state.comment);
-					}}
-				>
-					<InputGroup>
-						<FormControl
-							className="comment-input"
-							placeholder="Write a comment..."
-							as="textarea"
-							rows="2"
-							name="comment"
-							value={this.state.comment}
-							onChange={this.onChangeHandler}
-						/>
-						{/* <InputGroup.Append>
+    submitCommentHandler = (event) => {
+        event.preventDefault();
+        this.updateErrorStateWithCallback(this.props.submitComment);
+    };
+
+    validateCommentContent = () => {
+        return this.state.comment !== "";
+    };
+
+    isErrorFree = () => {
+        return this.state.chk.commentContentChk;
+    };
+
+    updateErrorStateWithCallback = (callback) => {
+        this.setState(
+            {
+                chk: { commentContentChk: this.validateCommentContent() },
+            },
+            () => {
+                if (this.isErrorFree()) {
+                    callback(this.state.comment);
+                }
+            }
+        );
+    };
+
+    render() {
+        return (
+            <div className="comment-form">
+                <hr></hr>
+                <Form
+                    onSubmit={(event) => {
+                        this.submitCommentHandler(event, this.state.comment);
+                    }}
+                >
+                    <InputGroup>
+                        <FormControl
+                            className="comment-input"
+                            placeholder="Write a comment..."
+                            as="textarea"
+                            rows="2"
+                            name="comment"
+                            value={this.state.comment}
+                            onChange={this.onChangeHandler}
+                        />
+                        {/* <InputGroup.Append>
 							<Button className="comment-submit-btn" type="submit" variant="primary">
 								Submit
 							</Button>
 						</InputGroup.Append> */}
-					</InputGroup>
-					<div className="comment-submit-btn-wrapper">
-						<Button className="comment-submit-btn" type="submit">
-							Comment
-						</Button>
-					</div>
-				</Form>
-			</div>
-		);
-	}
+                    </InputGroup>
+                    {this.state.chk.commentContentChk === false ? <Form.Text className="error">Comment content cannot be left blank.</Form.Text> : null}
+                    <div className="comment-submit-btn-wrapper">
+                        <Button className="comment-submit-btn" type="submit">
+                            Comment
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        );
+    }
 }
 
 export default CommentForm;
