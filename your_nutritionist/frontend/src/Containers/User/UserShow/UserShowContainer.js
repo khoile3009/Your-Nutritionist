c46b5e7887078b8a92c05ed7296822587b3ead50
Khoi Le <khoibaka@gmail.com>
2020-08-12 14:58:03 -0400

    Fix bug and add add refresh after signin

--- your_nutritionist/frontend/src/Containers/User/UserShow/UserShowContainer.js
+++ your_nutritionist/frontend/src/Containers/User/UserShow/UserShowContainer.js
@@ -97,12 +97,15 @@ class UserShowContainer extends Component {
 				})
 				.then((response) => {
 					let posts = this.state.posts;
-					posts.push.apply(posts, response.data.posts);
-					console.log(response.data.posts[response.data.posts.length - 1].post_id);
-					this.setState({
-						posts: posts,
-						last_id: response.data.posts[response.data.posts.length - 1].post_id,
-					});
+					if (response.data.posts && response.data.posts.length != 0) {
+						posts.push.apply(posts, response.data.posts);
+						console.log(response.data.posts[response.data.posts.length - 1].post_id);
+						this.setState({
+							posts: posts,
+							last_id: response.data.posts[response.data.posts.length - 1].post_id,
+						});
+					}
+
 					// this.setState({ posts: posts})
 				});
 		}
@@ -199,8 +202,8 @@ class UserShowContainer extends Component {
 						toPage={this.toPage}
 					></RecipeList>
 				) : (
-					<h3 style={{ color: "#757575" }}>No recipes</h3>
-				);
+						<h3 style={{ color: "#757575" }}>No recipes</h3>
+					);
 				break;
 			case "post":
 				content =
@@ -210,10 +213,10 @@ class UserShowContainer extends Component {
 							<PostCardList posts={this.state.posts} loadPosts={this.loadPosts} />
 						</>
 					) : (
-						<div className="no-content">
-							<h3 style={{ color: "#757575" }}>No posts</h3>
-						</div>
-					);
+							<div className="no-content">
+								<h3 style={{ color: "#757575" }}>No posts</h3>
+							</div>
+						);
 				break;
 			default:
 				content = this.state.user_recipes ? (
@@ -223,10 +226,10 @@ class UserShowContainer extends Component {
 						toPage={this.toPage}
 					></RecipeList>
 				) : (
-					<div className="no-content">
-						<h3 style={{ color: "#757575" }}>No recipes</h3>
-					</div>
-				);
+						<div className="no-content">
+							<h3 style={{ color: "#757575" }}>No recipes</h3>
+						</div>
+					);
 				break;
 		}
 		return this.state.user_info ? (
@@ -276,8 +279,8 @@ class UserShowContainer extends Component {
 				{this.props.token ? this.state.user_actions ? <ActionList actions={this.state.user_actions}></ActionList> : <h1>No action recently</h1> : <h1>You need to sign in to see this content</h1>} */}
 			</Container>
 		) : (
-			<NullPage></NullPage>
-		);
+				<NullPage></NullPage>
+			);
 	}
 }
 
@@ -288,4 +291,4 @@ const mapStateToProps = (state) => {
 	};
 };
 
-export default connect(mapStateToProps, () => {})(withRouter(UserShowContainer));
+export default connect(mapStateToProps, () => { })(withRouter(UserShowContainer));
